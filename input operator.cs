7f520b289593081f88f5f2eafa263c03f1e5a57a
girisha <girisha.devara@gmail.com>
2019-09-07 17:04:55 +0530

    raptor basics class

---
+++ input operator.cs
@@ -0,0 +1,20 @@
+using System;
+using System.IO;
+using System.Text;
+
+namespace input operator
+{
+   public class main_class
+   {
+      static System.Random random_generator = new System.Random();
+      public static void Main(string[] args)
+      {
+         string raptor_prompt_variable_zzyz;
+         ?? a;
+      
+         raptor_prompt_variable_zzyz ="Enter the value";
+         Console.WriteLine(raptor_prompt_variable_zzyz);
+         a= Double.Parse(Console.ReadLine());
+      }
+   }
+}
